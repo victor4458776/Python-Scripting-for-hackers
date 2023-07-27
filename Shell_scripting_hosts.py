e0938ef620f1cdcf73f97e8c052aae1cb3337bae
Victor Alonso <140429841+victor4458776@users.noreply.github.com>
2023-07-27 17:40:55 +0200

    Update Shell_scripting_hosts.py

--- Shell_scripting_hosts.py
+++ Shell_scripting_hosts.py
@@ -6,8 +6,37 @@
 ...        data = socket.gethostbyaddr('192.168.1.128')
 ...        host = data('8080')
 ...    except: 
-           host = '' 
+           host = '8080' 
        return host 
 
 #RECOVER NUMBER PROCESSING...
+>>> #EXECUTABLE NAME
+>>> proc_names={}
+>>> import psutil
+>>> for p in psutil.process_iter(attrs=['pid', 'name']):
+...    proc_names[p.info['pid']] = p.info['name']
+...    #LIST OF CONNECTIONS
+...    fmt = "{:25s}|{:25s}| {:20s}|{:15s}|{:s}" 
+
+>>> for p in psutil.process_iter(attrs=['pid', 'name']):
+...       fmt = "{:25s}|{:25s}| {:20s}|{:15s}|{:s}"
+...       title = fmt.format('Local', 'Distance','status','Process','Host')
+... 
+>>> print(title)
+Local                    |Distance                 | status              |Process        |Host
+
+>>> print(title)
+Local                    |Distance                 | status              |Process        |Host
+>>> print('='* len(title))
+==============================================================================================
+>>> for c in psutil.net_connections(kind='inet6'):
+...    connect = "%15s: %s" % (c.laddr[0], c.laddr[1])
+...    if c.raddr:
+...        r = "%15s: %s" % (c.raddr[0], c.raddr[1]) 
+...        host = resolve(c.raddr[0])
+...    else:
+...        r = ""
+...        host = ""
+...    s = c.status
+...    p = proc_names.get(c.pid, "")
 
